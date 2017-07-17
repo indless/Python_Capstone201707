a7fdfd6d296b4116b7d0f815df9d51559f60d973
Nick Hankins <nick.hankins@outlook.com>
2017-07-17 12:01:56 -0500

    Start of Python Capstone project

---
+++ Find_Me_The_Best_Job.py
@@ -0,0 +1,14 @@
+'''
+Create website to scrape leading job boards for new jobs that have the specified search criteria and
+categorize by key tags such as location, key words, titles, companies, posting date
+# would love to also create auto apply that will fill out various web forms and submit resume, create account and
+# store this in a database.
+
+# 1 scrape site for new job listing based on search criteria and tag
+# 2 create website with login to display scrape results with search features & store results in database
+# 3 auto apply for selected jobs
+# 4 integrate OpenAI
+
+# if possible, integrate OpenAI
+'''
+
---
+++ findjobs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FindjobsConfig(AppConfig):
+    name = 'findjobs'
