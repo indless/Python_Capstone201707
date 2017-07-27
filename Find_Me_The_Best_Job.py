37c4431da15d553f9bf54f8f2c482774256fa120
Nick Hankins <nick.hankins@outlook.com>
2017-07-27 18:08:11 -0500

    Start of Python Capstone project

--- Find_Me_The_Best_Job.py
+++ Find_Me_The_Best_Job.py
@@ -5,6 +5,8 @@ categorize by key tags such as location, key words, titles, companies, posting d
 # store this in a database.
 
 # 1 scrape site for new job listing based on search criteria and tag
+# 1.5 create form and checklists with entered search parameters
+# 1.6 tag search results for each of the search criteria they meet (SQL & Python, Python, SQL, Boston MA etc.)
 # 2 create website with login to display scrape results with search features & store results in database
 # 3 auto apply for selected jobs
 # 4 integrate OpenAI
